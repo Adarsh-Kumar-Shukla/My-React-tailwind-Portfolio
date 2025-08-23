e74f96b0ca83de6cc65ca8f9674b75a5d81a1939
Adarsh-Kumar-Shukla <adarshkshuklasrw@gmail.com>
2025-08-23 15:15:13 +0530

    Update AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1EDVFzZ972lWxOevzQV_R2t6zs_ZNyKob/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1zLGS2y_0dCJkOfn0NHKJkMnnx-FuD6NM/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
